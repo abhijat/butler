ffe7a17b7bd0efa535270cb58c4b82d3be2845ea
Abhijat Malviya <malviya.abhijat@gmail.com>
2018-06-02 22:57:10 +0530

    Adds crumb support to requests

--- auth_header_utils.go
+++ auth_header_utils.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func encodeUsernameAndPassword(username, password string) string {
@@ -14,4 +18,33 @@ func encodeUsernameAndPassword(username, password string) string {
 func addAuthHeader(request *http.Request, username string, password string) {
 	authHeader := fmt.Sprintf("Basic %s", encodeUsernameAndPassword(username, password))
 	request.Header.Add("Authorization", authHeader)
+}
+
+func acquireCredentials() (string, string) {
+	return os.Getenv("BUTLER_USER"), os.Getenv("BUTLER_PASSWORD")
+}
+
+func acquireCrumb() (string, string) {
+	url := `http://localhost:8080/crumbIssuer/api/json`
+	crumb, err := getURLContentAsString(url)
+	if err != nil {
+		log.Fatal("failed to acquire crumb from jenkins: ", err)
+	}
+
+	var response struct {
+		CrumbRequestField string
+		Crumb             string
+	}
+
+	err = json.NewDecoder(strings.NewReader(crumb)).Decode(&response)
+	if err != nil {
+		log.Fatalf("failed to parse crumb response: %v\n", err)
+	}
+
+	return response.CrumbRequestField, response.Crumb
+}
+
+func addCrumb(request *http.Request) {
+	crumbHeader, crumbValue := acquireCrumb()
+	request.Header.Set(crumbHeader, crumbValue)
 }
